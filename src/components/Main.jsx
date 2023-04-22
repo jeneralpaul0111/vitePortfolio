339fe0709102d9dcda8239ae3e81f2343dea2427
Paul Caballero <jeneralpaul0111@gmail.com>
2023-04-22 23:09:20 +0800

    first commit 1

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function main() {
+  return (
+    <div id='main'>
+            <img className='' src="./images/IMG_6314.jpg" alt="" />
+    </div>
+  )
+}
+
+export default main
